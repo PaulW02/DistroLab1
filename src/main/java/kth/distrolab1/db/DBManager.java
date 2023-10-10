0c5a9f73bc82c7fdf30bba71d6edb1a7d56e8459
PaulW02 <pslwi@ug.kth.se>
2023-10-10 15:50:21 +0200

    Last commit

--- src/main/java/kth/distrolab1/db/DBManager.java
+++ src/main/java/kth/distrolab1/db/DBManager.java
@@ -11,26 +11,14 @@ import java.sql.SQLException;
 public class DBManager {
     private static DBManager instance = null;
     private Connection con = null;
-
-    private static DBManager getInstance() {
-        if (instance == null)
-            instance = new DBManager();
-        return instance;
-    }
-
     private DBManager() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "1234");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public static Connection getConnection() {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "1234");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "123457");
         }catch(Exception ex){
             System.out.println(ex.getMessage());
             System.out.println("couldn't connect!");
